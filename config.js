bd4aa07d2436986c03216dcbf78998761db74722
malludg123 <malludg2@gmail.com>
2022-01-18 23:23:42 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '860rkhdn4fdvma', // your App ID
+      'clientSecret': 'GBPsFDSDYwCjwsho', // your App Secret
+      'callbackURL': 'http://localhost:3000/auth/linkedin/callback'
+    }
+  }
